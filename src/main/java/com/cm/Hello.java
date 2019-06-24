f9da9552064294a6ab70e87ce61124249008e996
wimsomiv19 <wimsomiv19@gmail.com>
2019-06-24 22:03:46 +0800

    java and kotlin variable, data type

--- src/main/java/com/cm/Hello.java
+++ src/main/java/com/cm/Hello.java
@@ -2,9 +2,17 @@ package com.cm;
 
 public class Hello {
     public static void main(String[] args) {
-//        System.out.println("Hello world");
         Person p = new Person();
         p.hello();
-
+        int age = 19;
+        Integer age2 = 19;
+        char c = '嘿';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Bear";
     }
 }
--- src/main/kotlin/com/cm/Hello.kt
+++ src/main/kotlin/com/cm/Hello.kt
@@ -3,10 +3,13 @@ package com.cm
 fun main() {
 //    println("Hello Kotlin")
 //    Human().hello()
-   val h = Human()
-        h.hello()
-//    h.hello()
-
+    val h = Human()
+    h.hello()
+    var age = 19
+    age = 20
+    var weight = 66.5f
+    var name : String
+    name = "Bear"
 }
 
 class Human{
